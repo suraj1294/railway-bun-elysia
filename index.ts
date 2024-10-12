a7f7dc0d9299bd2438ff339271f70807cdd3d91d
suraj <suraj@funnelstory.io>
2024-10-12 20:18:32 +0530

    check basic bun api

--- index.ts
+++ index.ts
@@ -1,49 +1,59 @@
-import { Elysia, t } from "elysia";
-import { swagger } from "@elysiajs/swagger";
+const server = Bun.serve({
+  hostname: "::",
+  port: process.env.PORT ?? 3000,
+  fetch(request) {
+    return new Response("Welcome to Bun!");
+  },
+});
 
-const app = new Elysia()
-  .use(swagger())
-  .get("/", () => ({ ok: "true" }))
-  .post(
-    "/sign-in",
-    async (context) => {
-      const { set, body } = context;
+console.log(`Listening on http://localhost:${server.port}`);
 
-      if (await signIn(body)) {
-        set.status = 200;
-        return { ok: "true" };
-      } else {
-        set.status = 403;
-        return { ok: "false", error: "invalid username or password" };
-      }
-    },
-    {
-      body: t.Object({
-        username: t.String(),
-        password: t.String(),
-      }),
-    }
-  )
-  .listen({
-    hostname: "::",
-    port: process.env.PORT ?? 3000,
-  });
+// import { Elysia, t } from "elysia";
+// import { swagger } from "@elysiajs/swagger";
 
-export type App = typeof app;
+// const app = new Elysia()
+//   .use(swagger())
+//   .get("/", () => ({ ok: "true" }))
+//   .post(
+//     "/sign-in",
+//     async (context) => {
+//       const { set, body } = context;
 
-function signIn(body: { username: string; password: string }): Promise<any> {
-  return new Promise((resolve) => {
-    if (body.username === "suraj" && body.password === "suraj1294") resolve(1);
-    else resolve(0);
-  });
-}
+//       if (await signIn(body)) {
+//         set.status = 200;
+//         return { ok: "true" };
+//       } else {
+//         set.status = 403;
+//         return { ok: "false", error: "invalid username or password" };
+//       }
+//     },
+//     {
+//       body: t.Object({
+//         username: t.String(),
+//         password: t.String(),
+//       }),
+//     }
+//   )
+//   .listen({
+//     hostname: "::",
+//     port: process.env.PORT ?? 3000,
+//   });
 
-// const server = Bun.serve({
-//   hostname: "::",
-//   port: process.env.PORT ?? 3000,
-//   fetch(request) {
-//     return new Response("Welcome to Bun!");
-//   },
-// });
+// export type App = typeof app;
 
-// console.log(`Listening on http://localhost:${server.port}`);
+// function signIn(body: { username: string; password: string }): Promise<any> {
+//   return new Promise((resolve) => {
+//     if (body.username === "suraj" && body.password === "suraj1294") resolve(1);
+//     else resolve(0);
+//   });
+// }
+
+// // const server = Bun.serve({
+// //   hostname: "::",
+// //   port: process.env.PORT ?? 3000,
+// //   fetch(request) {
+// //     return new Response("Welcome to Bun!");
+// //   },
+// // });
+
+// // console.log(`Listening on http://localhost:${server.port}`);
